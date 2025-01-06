39d4f4982c8821d2abb70379322c904760ac7b01
Ashvani Sharma <panditashuabvp@gmail.com>
2025-01-06 21:24:18 +0530

    Super and this keyword

---
+++ ThisAndSuperKW/Super.java
@@ -0,0 +1,37 @@
+// package ThisAndSuperKW;
+
+// class A{
+
+//     A(int c){
+//         System.out.println("Hello Super "+c);
+//     }
+
+//     int a=10;
+//     void disp(){
+//         System.out.println("Message from super class");
+//     }
+// }
+
+// class B extends A{
+
+//     B(){
+//         super(45);
+//         System.out.println("Hello Sub");
+//     }
+
+//     int a=20;
+//     void show(){
+//         System.out.println(a);
+//         System.out.println(super.a);
+//     }
+
+//     void disp(){
+//         super.disp();
+//         System.out.println("Message from sub class");
+//     }
+//     public static void main(String[] args) {
+//         B b=new B();
+//         b.show();
+//         b.disp();
+//     }
+// }
---
+++ ThisAndSuperKW/This.java
@@ -0,0 +1,23 @@
+// package ThisAndSuperKW;
+
+// class A{
+//     int a;
+//     A(){
+//         System.out.println("default");
+//     }
+//     A(int a){
+//         this();
+//        this.a=a;
+//        System.out.println(a);
+//     }
+//     public static void main(String[] args) {
+//         A r=new A(100);
+//        // System.out.println(r);
+//        // r.show();
+
+//     }
+
+//     void show(){
+//         System.out.println(a);
+//     }
+// }
