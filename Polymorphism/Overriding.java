69006b44367e0b773d69e64c0c2932f691720750
Ashvani Sharma <panditashuabvp@gmail.com>
2025-01-06 21:33:28 +0530

    Polymorphism

---
+++ Polymorphism/Overriding.java
@@ -0,0 +1,19 @@
+// class shape{
+//     public void draw(){
+//         System.out.println("Can't be say type of shape");
+//     }
+// }
+
+// class square extends shape{
+//     @Override
+//     public void draw(){
+//         System.out.println("Square shape");
+//     }
+// }
+
+// class Demo{
+//     public static void main(String[] args) {
+//         shape r=new square();
+//         r.draw();
+//     }
+// }
